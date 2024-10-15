9bedc3686a552579bcfd723d880ee01c5874b892
shreyesh93 <shreyesh.rane93@gmail.com>
2024-10-15 20:15:31 +0530

    Pattern 6 and 7 completed

--- Pattern.java
+++ Pattern.java
@@ -42,16 +42,36 @@ public class Pattern {
         // }
        
         // Pattern 5
-        int n=4;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
+        // int n=4;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
                 
-            System.out.print("\n");}
+        //     System.out.print("\n");}
+
+        //Pattern 6
+        for(int i=0;i<5;i++){
+            for(int j =0;j<=i;j++){
+                System.out.print(j+1);
+            }
+            System.out.print("\n");
+
+        }
+        
+        //Pattern 7
+        // int n = 5;
+        // for(int i=0;i<=n;i++){
+        //     for(int j =0;j<n-i;j++){
+        //         System.out.print(j+1);
+        //     }
+        //     System.out.print("\n");
+            
+
+        // }
 
 
 
