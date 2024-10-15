cd5156462d0f34882b401421b5e7bde59e5edaf0
shreyesh93 <shreyesh.rane93@gmail.com>
2024-10-15 20:57:20 +0530

    Pattern P1 completed

--- Pattern.java
+++ Pattern.java
@@ -1,4 +1,4 @@
-package JavaPat;
+package JavaPat.basic;
 
 public class Pattern {
 
@@ -88,23 +88,23 @@ public class Pattern {
         // }  
 
         //Pattern 9
-        int n = 5;
-        int k = 1;
-        for(int i =0 ;i<n;i++){
-            for(int j =0;j<=i;j++){
-                if(k%2==0){
-                System.out.print("0");
-                System.out.print(" ");}
-                else{
-                    System.out.print("1");
-                    System.out.print(" ");
-                }
-                k++;
+        // int n = 5;
+        // int k = 1;
+        // for(int i =0 ;i<n;i++){
+        //     for(int j =0;j<=i;j++){
+        //         if(k%2==0){
+        //         System.out.print("0");
+        //         System.out.print(" ");}
+        //         else{
+        //             System.out.print("1");
+        //             System.out.print(" ");
+        //         }
+        //         k++;
                 
-            }
+        //     }
             
-            System.out.print("\n");
-        }
+        //     System.out.print("\n");
+        // }
         
 
 
