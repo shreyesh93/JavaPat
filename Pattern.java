53fff05969dc12a9e290863bfa1969c3b1e8ac98
shreyesh93 <shreyesh.rane93@gmail.com>
2024-10-15 18:02:55 +0530

    Pattern1 completed

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+package JavaPat;
+
+public class Pattern {
+
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j =0;j<5;j++){
+                System.out.print("*");
+            }
+        System.out.print("\n");}
+
+
+        }
+    }
