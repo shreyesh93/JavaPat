9151a940411fb4dfe0cdcea8288b01494cd779d7
shreyesh93 <shreyesh.rane93@gmail.com>
2024-10-15 18:38:18 +0530

    Pattern3 completed

--- Pattern.java
+++ Pattern.java
@@ -10,19 +10,27 @@ public class Pattern {
         //     }
         // System.out.print("\n");}
 
-        // pattern 2
-        for(int i=0;i<5;i++){
-            for(int j =0;j<5;j++){
-                if(i>0 && i<4 && j>0 && j<4 ){
-                    System.out.print(" ");
+        // Pattern 2
+        // for(int i=0;i<5;i++){
+        //     for(int j =0;j<5;j++){
+        //         if(i>0 && i<4 && j>0 && j<4 ){
+        //             System.out.print(" ");
 
-                }
-                else{
-                System.out.print("*");}
+        //         }
+        //         else{
+        //         System.out.print("*");}
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        // Pattern 3
+        for(int i=0;i<5;i++){
+            for(int j =0;j<i+1;j++){
+                System.out.print("*");
+                
             }
             System.out.print("\n");
         }
-
 
 
 
