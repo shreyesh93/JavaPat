e525371519754b95377252d3798a5f2a16e2503f
shreyesh93 <shreyesh.rane93@gmail.com>
2024-10-15 18:17:04 +0530

    Pattern2 completed

--- Pattern.java
+++ Pattern.java
@@ -3,11 +3,27 @@ package JavaPat;
 public class Pattern {
 
     public static void main(String[] args) {
+        //pattern 1
+        // for(int i=0;i<5;i++){
+        //     for(int j =0;j<5;j++){
+        //         System.out.print("*");
+        //     }
+        // System.out.print("\n");}
+
+        // pattern 2
         for(int i=0;i<5;i++){
             for(int j =0;j<5;j++){
-                System.out.print("*");
+                if(i>0 && i<4 && j>0 && j<4 ){
+                    System.out.print(" ");
+
+                }
+                else{
+                System.out.print("*");}
             }
-        System.out.print("\n");}
+            System.out.print("\n");
+        }
+
+
 
 
         }
