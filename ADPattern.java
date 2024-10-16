e3b1bde4ec2cc59468f0769b3d98c60b6a10b599
shreyesh93 <shreyesh.rane93@gmail.com>
2024-10-16 14:59:48 +0530

    AD pattern 2 completed

--- ADPattern.java
+++ ADPattern.java
@@ -5,60 +5,63 @@ package JavaPat;
 public class ADPattern {
 
     public static void main(String[] args) {
+
+        // ADQ1
+
         // int n = 4;
-        // for(int i = 0;i<n;i++){
-        //     for(int j=0;j<=i;j++){
+        // int k = 8;
+        // int kut = 2;
+        // for(int i = 1;i<n+1;i++){
+        //     for(int j=1;j<=i;j++){
         //         System.out.print("*");
         //     }
-        //     for(int j = 5;j<=5;j++){
-                
-        //         System.out.print("p");
-                
-                
+        //     if(i<n){
+        //         for(int j=2;j<k;j++){
+        //             System.out.print(" ");
         //     }
-        //     System.out.print("\n");
-        // }
-    
-        int n = 4;
-        int k = 8;
-        int kut = 2;
-        for(int i = 1;i<n+1;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-        if(i<n){
-        for(int j=2;j<k;j++){
-            System.out.print(" ");
-        }
-    k-=2;}
+        //             k -=2;}
            
 
-    for(int j=1;j<=i;j++){
-        System.out.print("*");
-    }
+        // for(int j=1;j<=i;j++){
+        //     System.out.print("*");
+        // }
+        
+        //     System.out.println();
     
-        System.out.println();
     
+        // }
     
-    }
-    
-    for(int i = 1;i<5;i++){
-            for(int j=5;j>=i+1;j--){
-                System.out.print("*");
-            }
-            if(i>=1 && i<5){
-                    for(int j=2;j<kut;j++){
-                        System.out.print(" ");
-                    }
-                kut +=2;}
-                for(int j=5;j>=i+1;j--){
-                    System.out.print("*");
-                }
+        // for(int i = 1;i<5;i++){
+
+        //     for(int j=5;j>=i+1;j--){
+        //         System.out.print("*");
+        //     }
+        //     if(i>=1 && i<5){
+        //             for(int j=2;j<kut;j++){
+        //                 System.out.print(" ");
+        //             }
+        //         kut +=2;}
+        //         for(int j=5;j>=i+1;j--){
+        //             System.out.print("*");
+        //         }
             
-            System.out.println();
+        //     System.out.println();
+            
+        // }
+
+        //ADQ2
+        int n = 4;
+        for(int i = 1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            
+            for(int j =1;j<=i;j++){
+                System.out.print(i);
             
         }
+               System.out.println();
+}
 
-    
 
 }}
