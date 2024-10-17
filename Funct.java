effa7a1d5c6ca91fbd45fdd605486d7887a5fbc4
shreyesh93 <shreyesh.rane93@gmail.com>
2024-10-17 19:26:16 +0530

    Q2 done

--- Funct.java
+++ Funct.java
@@ -1,17 +1,34 @@
+package JavaPat;
+import java.util.Scanner;
 
+public class Funct {
 
+    //1. using function to print average of 3 numbers
+    
+    // int A,B,C;
+    // public void j(int A,int B,int C){
+    //     int k = A + B + C;
+    //     System.out.println(k/3);
+    // }
+    // public static void main(String[] args) {
+    //     Funct jo = new Funct();
+    //     jo.j(1,2,3);  }
 
-public class JavaAp {
-    int A,B,C;
-    public void j(int A,int B,int C){
-        int k = A + B + C;
-        System.out.println(k/3);
+    //2. function to print sum of all odd numbers from 1 to n
+    int k = 0;
+    public void summ(int n){
+    for(int i=0;i<=n;i++){
+        if(i%2 != 0){
+            k += i;
+        }}
+        System.out.println("sum of odd number till"+" "+n+" "+ "is" +" "+k);
     }
     public static void main(String[] args) {
-        JavaAp jo = new JavaAp();
-        jo.j(1,2,3);
-        
-        
-       
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number till which u have to print sum of odd number:");
+        int n = sc.nextInt();
+        Funct f = new Funct();
+        f.summ(n);    
     }
-}
+
+    }
