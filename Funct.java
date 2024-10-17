a3b1c47b6727b37985d382319d66f9c1cb530892
shreyesh93 <shreyesh.rane93@gmail.com>
2024-10-17 19:48:39 +0530

    Q3 done

--- Funct.java
+++ Funct.java
@@ -14,21 +14,66 @@ public class Funct {
     //     Funct jo = new Funct();
     //     jo.j(1,2,3);  }
 
+
     //2. function to print sum of all odd numbers from 1 to n
-    int k = 0;
-    public void summ(int n){
-    for(int i=0;i<=n;i++){
-        if(i%2 != 0){
-            k += i;
-        }}
-        System.out.println("sum of odd number till"+" "+n+" "+ "is" +" "+k);
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number till which u have to print sum of odd number:");
-        int n = sc.nextInt();
-        Funct f = new Funct();
-        f.summ(n);    
+
+    // int k = 0;
+    // public void summ(int n){
+    // for(int i=0;i<=n;i++){
+    //     if(i%2 != 0){
+    //         k += i;
+    //     }}
+    //     System.out.println("sum of odd number till"+" "+n+" "+ "is" +" "+k);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Enter a number till which u have to print sum of odd number:");
+    //     int n = sc.nextInt();
+    //     Funct f = new Funct();
+    //     f.summ(n);    
+    // }
+
+
+    //
+    int l,o,p = 0;
+    Scanner sc = new Scanner(System.in);
+    public void ff(){
+        
+        System.out.println("Enter how many numbers:");
+        int a = sc.nextInt();
+        int [] arr= new int[a];
+        for(int i = 0;i<a;i++){
+            System.out.println("Enter number" +" "+ i + " "+":");
+            int n = sc.nextInt();
+            arr[i] = n;
+
+        }
+        System.out.println();
+        System.out.println("elements in array are:");
+        for(int kk:arr){
+            
+            System.out.print(kk);
+            System.out.print(" ");
+        }
+        for(int b:arr){
+            if(b==0){
+                l++;
+            }
+            else if(b>0){
+                o++;
+            }
+            else{
+                p++;
+            }
+        }
+        System.out.println("\nNumber of zeros are"+" "+l+ "\n"+ "Number of positive are"+" "+o+ "\n"+"Number of zeros are"+" "+p);
+        
     }
 
+    public static void main(String[] args) {
+        Funct f = new Funct();
+        f.ff();
+        }
     }
+
+    
